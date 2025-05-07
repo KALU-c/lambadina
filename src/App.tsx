2ed83cd67466c06722db3d6eafecc24959285b0f
Endekalu Zemenu <endekaluzemenu2134@gmail.com>
2025-05-07 20:12:54 +0300

    feat: add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
+import Footer from './components/Footer'
 import Home from './components/Home'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
+import { Button } from './components/ui/button'
+import { Input } from './components/ui/input'
 
 function App() {
 
   return (
-    <main className='px-[22px] pt-[10px] flex flex-col space-y-5'>
-      <Navbar />
-      <Home />
+    <main className='pt-[10px] flex flex-col'>
+      <div className='px-[22px] flex flex-col space-y-5 mb-10'>
+        <Navbar />
+        <Home />
+      </div>
+      <div className="flex flex-col justify-center gap-6 bg-[url('/bg.jpg')] h-[380px] w-full px-10">
+        <p className="text-3xl text-center text-white">Sign up free as a client or advisor</p>
+
+        <div className="flex flex-col gap-3">
+          <Input className='bg-white h-16 focus-visible:right-0 shadow-none' placeholder='Enter your email' type='email' />
+          <Button className='h-14 bg-[#FFB000] text-lg'>
+            Submit
+          </Button>
+        </div>
+      </div>
+      <Footer />
     </main>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,68 @@
+import {
+  SiInstagram,
+  SiX
+} from '@icons-pack/react-simple-icons'
+import { Separator } from './ui/separator'
+
+const Footer = () => {
+  return (
+    <div className="bg-[#0C0C0C] text-white px-6 py-8 space-y-4">
+      <div className='py-6'>
+        <span className='text-2xl'>LAMBADINA</span>
+      </div>
+
+      <p className='text-2xl font-extralight pr-10 text-[#E8E4DF]'>
+        Book the most in-demand experts & get advice over a video call
+      </p>
+
+      <div className='flex flex-col gap-4 py-6'>
+        <span className='font-semibold text-xl'>Follow us</span>
+        <div className='flex flex-row gap-4 items-center'>
+          <div className='p-3 rounded-full bg-white'>
+            <SiInstagram color='#000000' />
+          </div>
+          <div className='p-3 rounded-full bg-white'>
+            <SiX color='#000000' />
+          </div>
+        </div>
+      </div>
+
+      <Separator className='bg-[#D8D8D8]' />
+
+      <div className='flex flex-col gap-4 py-4'>
+        <p className='font-semibold text-xl'>Company</p>
+
+        <div className='flex flex-col gap-3'>
+          <span className='text-lg text-[#E8E4DF] font-light'>About</span>
+          <span className='text-lg text-[#E8E4DF] font-light'>Careers</span>
+          <span className='text-lg text-[#E8E4DF] font-light'>FAQ</span>
+          <span className='text-lg text-[#E8E4DF] font-light'>Gift a session</span>
+          <span className='text-lg text-[#E8E4DF] font-light'>Experts</span>
+        </div>
+      </div>
+
+      <Separator className='bg-[#D8D8D8]' />
+
+      <div className='flex flex-col gap-4 py-4'>
+        <p className='font-semibold text-xl'>Support</p>
+
+        <div className='flex flex-col gap-3'>
+          <span className='text-lg text-[#E8E4DF] font-light'>Contact</span>
+          <span className='text-lg text-[#E8E4DF] font-light'>Give us feedback & earn</span>
+          <span className='text-lg text-[#E8E4DF] font-light'>Suggest a feature & earn</span>
+          <span className='text-lg text-[#E8E4DF] font-light'>Suggest a new topic or export</span>
+          <span className='text-lg text-[#E8E4DF] font-light'>Policy</span>
+          <span className='text-lg text-[#E8E4DF] font-light'>Terms</span>
+        </div>
+      </div>
+
+      <Separator className='bg-[#D8D8D8]' />
+
+      <span className='text-sm font-light'>
+        @ Lambadina 2025 {new Date().getFullYear()}. All rights reserved
+      </span>
+    </div>
+  )
+}
+
+export default Footer
